6436390dfa1aec16978a5a0accd9c1bfb0ca75ec
zan <zan_zibert@hotmail.com>
2019-06-01 14:37:25 +0200

    square roots with newtons method

--- main.scala
+++ main.scala
@@ -2,5 +2,20 @@ object ScalaTutorial {
     def main(args: Array[String]) {
         
     }
-    def abs(x: Int) = if (x >= 0) x else -x
+
+    def abs(x: Double) = if (x >= 0) x else -x
+
+    def sqrt(x: Double): Double = sqrtIter(1.0, x)
+
+    def sqrtIter(guess: Double, x: Double): Double =
+      if (isGoodEnough(guess, x)) guess
+      else sqrtIter(improve(guess, x), x)
+    
+    def isGoodEnough(guess: Double, x: Double) = 
+      abs((guess * guess) - x) < 0.001
+
+    def improve(guess: Double, x: Double) =
+      (guess + x / guess) / 2
+
+    println(sqrt(25))
 }
