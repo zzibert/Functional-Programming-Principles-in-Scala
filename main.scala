0ab9d50ec8119d6d21ca7e95c1132d31e3a1a4d9
zan <zan_zibert@hotmail.com>
2019-06-01 15:05:09 +0200

    blocks and lexical scope

--- main.scala
+++ main.scala
@@ -5,17 +5,19 @@ object ScalaTutorial {
 
     def abs(x: Double) = if (x >= 0) x else -x
 
-    def sqrt(x: Double): Double = sqrtIter(1.0, x)
+    def sqrt(x: Double): Double = {
+      def sqrtIter(guess: Double): Double =
+        if (isGoodEnough(guess)) guess
+        else sqrtIter(improve(guess))
+      
+      def isGoodEnough(guess: Double) = 
+        abs((guess * guess) - x) < 0.001
 
-    def sqrtIter(guess: Double, x: Double): Double =
-      if (isGoodEnough(guess, x)) guess
-      else sqrtIter(improve(guess, x), x)
-    
-    def isGoodEnough(guess: Double, x: Double) = 
-      abs((guess * guess) - x) < 0.001
+      def improve(guess: Double) =
+        (guess + x / guess) / 2
 
-    def improve(guess: Double, x: Double) =
-      (guess + x / guess) / 2
+      sqrtIter(1)
+    }
 
-    println(sqrt(25))
+    println(sqrt(16))
 }
