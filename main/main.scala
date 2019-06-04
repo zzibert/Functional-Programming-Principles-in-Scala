6571f6b5ddef56e2d071cf53b26c0574454f3e90
zan <zan_zibert@hotmail.com>
2019-06-04 22:03:28 +0200

    higher order functions as mapReduce

--- main/main.scala
+++ main/main.scala
@@ -8,13 +8,22 @@ object Main extends App {
         }
         loop(a, 0)
     }
-    def product(f: Int => Int)(a: Int, b: Int): Int = {
-      if (a > b) 1
-      else f(a) * product(f)(a + 1, b)
-    }
+    def product(f: Int => Int)(a: Int, b: Int): Int = generalized((x, y) => x * y)(1)(f)(a, b)
 
     def factorial(a: Int) = product(x => x)(1, a)
 
+    def generalized(f: (Int, Int) => Int)(unit: Int)(g: Int => Int)(a: Int, b: Int): Int = {
+      if (a > b) unit
+      else f(g(a), generalized(f)(unit)(g)(a + 1, b))
+    }
+
+    // summation
+    println(generalized((x, y) => x + y)(0)(x => x)(1, 10))
+
+    // multiplication
+    println(generalized((x, y) => x * y)(1)(x => x)(1, 5))
+
+    //factorial defined as product defined as generalization
     println(factorial(6))
     
   
