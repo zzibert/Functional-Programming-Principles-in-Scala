d08d3779a25909fc0066345d526b749e11bae7b3
zan <zan_zibert@hotmail.com>
2019-06-08 12:53:50 +0200

    more fun with rationals

--- main/main.scala
+++ main/main.scala
@@ -1,25 +1,34 @@
 package main
 
 object Main extends App {
-  val x = new Rational(1, 3)
-  val y = new Rational(5, 7)
-  val z = new Rational(3, 2)
+  val x = new Rational(70, 42)
+  val y = new Rational(6, 3)
+  val z = new Rational(4, 3)
+  
 
-  println(x.sub(y).sub(z))
-
+  println(x.less(y))
+  println(x.less(z))
+  println(new Rational(2))
 }
 
 class Rational(x: Int, y: Int) {
-    def numer = x
-    def denom = y
-    def add(that: Rational) = 
-      new Rational(
-          numer * that.denom + that.numer * denom,
-          denom * that.denom
-      )
-    def neg = new Rational(-numer, denom)
-    def sub(that: Rational) = add(that.neg)
-    
-    override def toString = numer + "/" + denom
+  require(y != 0, "denominator must be nonzero")
+
+  def this(x: Int) = this(x, 1)
+  private def gcd(a: Int, b: Int): Int = if(b == 0) a else gcd(b, a % b)
+  private val g = gcd(x, y)
+  def numer = x / g
+  def denom = y / g
+  def add(that: Rational) = 
+    new Rational(
+        numer * that.denom + that.numer * denom,
+        denom * that.denom
+    )
+  def neg = new Rational(-numer, denom)
+  def sub(that: Rational) = add(that.neg)
+  def less(that: Rational) =  ((numer / denom) < (that.numer /that.denom))
+  def max(that: Rational) = if (this.less(that)) that else this
+  
+  override def toString = numer + "/" + denom
 }
 
