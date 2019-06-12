a55e392d9c0c268023bd06fdaaeae6de6f751e9f
zan <zan_zibert@hotmail.com>
2019-06-12 21:46:58 +0200

    polymorphism

--- main/main.scala
+++ main/main.scala
@@ -1,46 +1,15 @@
 package main
 
-object Main extends App {
-  val t1 = new NonEmpty(1, new Empty, new Empty)
-  val t2 = t1 incl 0
-  val t3 = t2 incl 3
+import week4._
 
-  val t4 = new NonEmpty(2, new Empty, new Empty)
-  val t5 = t4 incl 1
-  val t6 = t5 incl 4
+object nth {
+    def nth[T](n: Int, xs: List[T]): T =
+      if (xs.isEmpty) throw new IndexOutOfBoundException
+      else if (n == 0) xs.head
+      else nth(n - 1, xs.tail)
 
-  val t7 = t3 union t6
-  
-  
+    val list = new Cons(1, new Cons(2, new Cons(3, new Nil)))
 
-  println(t6)
-}
+    nth(2, list)
 
-abstract class IntSet {
-  def incl(x: Int): IntSet
-  def contains(x: Int): Boolean
-  def union(other: IntSet): IntSet
-}
-
-class Empty extends IntSet {
-  def contains(x: Int): Boolean = false
-  def incl(x: Int): IntSet = new NonEmpty(x, new Empty, new Empty)
-  def union(other: IntSet) = other
-  override def toString = "."
-}
-
-class NonEmpty(elem: Int, left: IntSet, right: IntSet) extends IntSet {
-  def contains(x: Int): Boolean =
-    if (x < elem) left contains x
-    else if (x > elem) right contains x
-    else true
-  def incl(x: Int): IntSet =
-    if (x < elem) new NonEmpty(elem, left incl x, right)
-    else if ( x > elem) new NonEmpty(elem, left, right incl x)
-    else this
-  def union(other: IntSet): IntSet = {
-    ((left union right) union other) incl elem
-  }
-
-  override def toString = "{ " + left  + elem +  right + " }" 
 }
